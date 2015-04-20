e267755fe694e173fd560ad36c1ea4b76bd14686
ChaseMacek <cmacek@itriagehealth.com>
2015-04-20 13:22:59 -0600

    add updated thr ruby

--- thr.rb
+++ thr.rb
@@ -1,20 +1,24 @@
 require "csv"
+
+
 def my_replace column, old_value, new_value
+
+
    return column.gsub!(old_value, new_value) if column
+
 end
+
 
 old_file = CSV.open("texas_health.csv")
 new_file = CSV.open("thr_corrected.csv", "w")
 line = old_file.readline
-newline = Array.new
-line.each do |item|
-  newline.push (item.downcase)
-end
-new_file << newline
+line.pop(5)
+new_file << line
 new_file.close
 old_file.close
 
 contents = CSV.open "texas_health.csv", headers: true, header_converters: :symbol
+
 contents.each do |row|
 
   specialty = row[:specialty_1]
@@ -23,6 +27,7 @@ contents.each do |row|
   specialty4 = row[:specialty_4] || " "
   specialty5 = row[:specialty_5] || " "
 
+    # only fill in the .gsub methods
 if row[:sponsor_specialty_1] && row[:sponsor_specialty_1].length > 0
   row[:specialty_1] = row[:sponsor_specialty_1]
   row[:specialty_2] = row[:sponsor_specialty_2]
@@ -34,6 +39,10 @@ if row[:sponsor_specialty_1] && row[:sponsor_specialty_1].length > 0
   row[:sponsor_specialty_4] = ""
   row[:sponsor_specialty_5] = ""
 end
+
+
+  # specialty_correction = specialty.gsub(/Cardiovascular Disease/, "heart stuff")
+  # .gsub(/Family Medicine/, "fam stuff")
 
   specialty_correction = specialty
   specialty_correction2 = specialty2
@@ -60,6 +69,7 @@ end
    my_replace obj, /Family Medicine and OMT/, "Family Medicine"
    my_replace obj, /Joint Replacement/, "Joint Replacement Surgery"
    my_replace obj, /Joint Replacement Surgery Surgery/, "Joint Replacement Surgery"
+
   end
 
  row.delete(:sponsor_specialty_5)
@@ -68,7 +78,27 @@ end
  row.delete(:sponsor_specialty_2)
  row.delete(:sponsor_specialty_1)
 
+#  specialty_correction = my_replace specialty, /Cards/, "Cardiovascular Disease"
+  #puts specialty_correction3
+#  specialty_correction2 = my_replace specialty2, /Cardiovascular Disease/, "heart stuff"
+#  specialty_correction3 = my_replace specialty3, /Cardiovascular Disease/, "heart stuff"
+#  specialty_correction4 = my_replace specialty4, /Cardiovascular Disease/, "heart stuff"
+
+
+#  specialty_correction2 = specialty2.gsub(/Cardiovascular Disease/, "heart stuff")
+#  .gsub(/Interventional Cardiology/, "saved heart stuff")
+
+#  specialty_correction3 = specialty3.gsub(/Cardiovascular Disease/, "heart stuff")
+#  .gsub(/orthopedic surgery/, "joint stuff")
+
+#  specialty_correction4 = specialty4.gsub(/Interventional Cardiology/, "more heart stuff")
+#  .gsub(/Clinical Cardiac Electrophysiology/, "electric stuff?")
+
+#  specialty_correction5 = specialty5.gsub(/Cardiovascular Disease/, "still more heart stuff")
+#  .gsub(/Cardiovascular Disease/, "still more heart stuff")
+
   CSV.open("thr_corrected.csv", "a+") do |csv|
+#    csv << [specialty_correction, specialty_correction2, specialty_correction3, specialty_correction4, specialty_correction5]
     csv << row
   end
 end
